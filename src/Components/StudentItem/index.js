68b30dc904eb972fc34372cc09f5502ce92d2227
aidanakmatova13 <akmatova.don@gmail.com>
2021-09-22 03:04:22 +0300

    fix all code

--- src/Components/StudentItem/index.js
+++ src/Components/StudentItem/index.js
@@ -1,22 +1,120 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {useForm} from "react-hook-form";
 
-const StudentItem = ({el, idx, delData, setIsOpenEdit}) => {
+
+const StudentItem = ({el, idx, delData, updateStudent}) => {
+    const [showModal, setShowModal] = useState(false)
+    const {register, handleSubmit, formState: {errors}, reset} = useForm()
+
+    const saveData = (data) => {
+        updateStudent(el.id, data)
+        setShowModal(false)
+    }
     return (
-            <tr>
-                <th scope="row">{idx+1}</th>
-                <td>{el.name}</td>
-                <td>{el.phone}</td>
-                <td>{el.contract}</td>
-                <td>{el.ispaid}</td>
-                <td>{el.status}</td>
-                <td>{el.group}</td>
-                <td>{el.notebook}</td>
-                <td>{el.comments}</td>
-                <td>
-                    <button className='edit-btn' onClick={() => setIsOpenEdit(true)}>Редактировать</button>
-                    <button className='del-btn' onClick={() => delData(el.id)}>Удалить</button>
-                </td>
-            </tr>
+        <>
+        <tr>
+            <th scope="row">{idx + 1}</th>
+            <td>{el.name}</td>
+            <td>{el.phone}</td>
+            <td>{el.contract}</td>
+            <td>{el.ispaid}</td>
+            <td>{el.status}</td>
+            <td>{el.group}</td>
+            <td>{el.gender}</td>
+            <td>{el.notebook}</td>
+            <td>{el.comments}</td>
+            <td>
+                <button className='edit-btn' onClick={() => setShowModal(true)}>Редактировать</button>
+                <button className='del-btn' onClick={() => delData(el.id)}>Удалить</button>
+            </td>
+        </tr>
+    {showModal && <div className="modal-window">
+        <div className="modal-content">
+            {showModal &&
+            <form onSubmit={handleSubmit(saveData)}>
+                <div className="form-group d-flex justify-content-between">
+                    <label>Ваше имя:
+                        <input defaultValue={el.name} name='name' className='form-input' type="text"
+                               {...register('name', {required: true})}
+                        />
+                    </label>
+                    {errors.name && <div className='text-red'>Заполните поле*</div>}
+
+                    <label>Ваш телефон:
+                        <input defaultValue={el.phone} name='phone' className='form-input' type="number"
+                               {...register('phone', {required: true})}
+                        />
+                    </label>
+                    {errors.phone && <div className='text-red'>Заполните поле*</div>}
+                </div>
+                <div className="form-group d-flex justify-content-between">
+                    <label>Контракт:
+                        <input defaultValue={el.contract} name='contract' className='form-input' type="number"
+                               {...register('contract', {required: true})}
+                        />
+                    </label>
+                    {errors.contract && <div className='text-red'>Заполните поле*</div>}
+
+                    <label>Оплачено:
+                        <input defaultValue={el.ispaid} name='ispaid' className='form-input' type="number"
+                               {...register('ispaid', {required: true})}
+                        />
+                    </label>
+                    {errors.ispaid && <div className='text-red'>Заполните поле*</div>}
+                </div>
+                <div className="form-group">
+                    <label>Ноутбук:
+                        <input defaultValue={el.notebook} name='notebook' className='form-input' type="text"
+                               {...register('notebook', {required: true})}
+                        />
+                    </label>
+                    {errors.notebook && <div className='text-red'>Заполните поле*</div>}
+                </div>
+                <div className="form-group d-flex justify-content-between">
+                    <label htmlFor="group">Выберите группу:
+                        <div><select {...register('group', {required: true})} >
+                            <option selected>Выберите группу</option>
+                            <option value="Утренняя">Утренняя</option>
+                            <option value="Вечерняя">Вечерняя</option>
+                        </select>
+                        </div>
+                    </label>
+                    {errors.group && <div className='text-red'>Выберите*</div>}
+                    <label htmlFor="gender">Выберите пол:
+                        <div><select {...register('gender', {required: true})} >
+                            <option selected>Выберите пол</option>
+                            <option value="мужской">Мужской</option>
+                            <option value="женский">Женский</option>
+                        </select>
+                        </div>
+                    </label>
+                    {errors.gender && <div className='text-red'>Выберите*</div>}
+                    <label htmlFor="status">Выберите статус:
+                        <div><select {...register('status', {required: true})} >
+                            <option selected>Выберите статус</option>
+                            <option value="active">active</option>
+                            <option value="off">off</option>
+                        </select>
+                        </div>
+                    </label>
+                    {errors.status && <div className='text-red'>Выберите статус*</div>}
+                </div>
+
+                <div className="form-group">
+                    <label htmlFor="comments">Ваш комментарий:</label>
+                    <textarea defaultValue={el.comments} name="comments" id="comments" style={{width: "100%"}} cols='5'
+                              placeholder='Комментарий...'
+                              {...register('comments')}
+                    />
+                    {errors.comments && <div className='text-red'>Заполните поле*</div>}
+                </div>
+                <button className='send-btn' type="submit">Сохранить</button>
+            </form>
+            }
+            <button onClick={() => setShowModal(false)} className="close-modal">&#215;</button>
+        </div>
+    </div>}
+        </>
     );
 };
 
--- src/Views/Students/index.js
+++ src/Views/Students/index.js
@@ -7,40 +7,44 @@ import Spinner from "../../Components/Spinner";
 import StudentItem from "../../Components/StudentItem";
 
 
-const Students = () =>{
-    const [students,setStudents] = useState([]) //чтобы значение меняласьи выводилось визуально
+const Students = () => {
+    const [students, setStudents] = useState([]) //чтобы значение меняласьи выводилось визуально
     const [isOpen, setIsOpen] = useState(false)
-    const [isOpenEdit, setIsOpenEdit] = useState(false)
     const {register, handleSubmit, formState: {errors}, reset} = useForm()
     const [isLoading, setIsLoading] = useState(true)
 
 
-    useEffect(() =>{
+    useEffect(() => {
         axios('https://613d36a694dbd600172ab88f.mockapi.io/api/students')
             .then(({data}) => {
                 setStudents(data)
                 setIsLoading(false)
             })
-    },[])
+    }, [])
 
-    const sendData = (data) =>{
+    const sendData = (data) => {
         axios.post('https://613d36a694dbd600172ab88f.mockapi.io/api/students', data)
-            .then(({data:student}) =>{
+            .then(({data: student}) => {
                 setStudents([...students, student])
                 setIsOpen(false)
                 reset()
             })
     }
 
-    const delData = (id) =>{
+    const delData = (id) => {
         axios.delete(`https://613d36a694dbd600172ab88f.mockapi.io/api/students/${id}`)
             .then(() => setStudents(students.filter(item => item.id !== id)))
     }
 
-    if (isLoading){
+    const updateStudent = (id, obj) =>{
+        axios.put(`https://613d36a694dbd600172ab88f.mockapi.io/api/students/${id}`, obj)
+            .then(() => setStudents(students.map(item => item.id === id ? {...item, ...obj} : item)))
+    }
+
+    if (isLoading) {
         return <Spinner/>
     }
-    return(
+    return (
         <Layout>
             <div className='container'>
                 <table className="table my-5">
@@ -53,74 +57,105 @@ const Students = () =>{
                         <th scope="col">Оплачено</th>
                         <th scope="col">Статус</th>
                         <th scope="col">Группа</th>
+                        <th scope="col">Пол</th>
                         <th scope="col">Ноутбук</th>
                         <th scope="col">Комментарии</th>
+                        <th scope="col">Действия</th>
                     </tr>
                     </thead>
                     <tbody>
                     {
-                        students.map((el,idx) =>
-                            <StudentItem key={el.id} el={el} idx={idx} delData={delData} setIsOpenEdit={setIsOpenEdit}/>
+                        students.map((el, idx) =>
+                            <StudentItem key={el.id} el={el} idx={idx} delData={delData} updateStudent={updateStudent}/>
                         )
                     }
                     </tbody>
                 </table>
-                <button onClick={() => setIsOpen(true)} className='add-btn' >Добавить нового студента</button>
+                <button onClick={() => setIsOpen(true)} className='add-btn'>Добавить нового студента</button>
             </div>
             {isOpen && <div className="modal-window">
                 <div className="modal-content">
                     {
                         isOpen &&
-                            <form onSubmit={handleSubmit(sendData)}>
-                                <div className="form-group d-flex justify-content-between">
-                                    <label>Ваше имя:
+                        <form onSubmit={handleSubmit(sendData)}>
+                            <div className="form-group d-flex justify-content-between">
+                                <label>Ваше имя:
                                     <input name='name' className='form-input' type="text"
-                                           {...register('name', {required:true})}
+                                           {...register('name', {required: true})}
                                     />
-                                    </label>
-                                    {errors.name && <div className='text-red'>Заполните поле*</div>}
+                                </label>
+                                {errors.name && <div className='text-red'>Заполните поле*</div>}
 
-                                    <label>Ваш телефон:
-                                    <input name='phone' className='form-input'  type="number"
-                                           {...register('phone', {required:true})}
+                                <label>Ваш телефон:
+                                    <input name='phone' className='form-input' type="number"
+                                           {...register('phone', {required: true})}
                                     />
-                                    </label>
-                                    {errors.phone && <div className='text-red'>Заполните поле*</div>}
-                                </div>
-                                <div className="form-group d-flex justify-content-between">
-                                    <label>Контракт:
-                                    <input name='contract' className='form-input'  type="number"
-                                           {...register('contract', {required:true})}
+                                </label>
+                                {errors.phone && <div className='text-red'>Заполните поле*</div>}
+                            </div>
+                            <div className="form-group d-flex justify-content-between">
+                                <label>Контракт:
+                                    <input name='contract' className='form-input' type="number"
+                                           {...register('contract', {required: true})}
                                     />
-                                    </label>
-                                    {errors.contract && <div className='text-red'>Заполните поле*</div>}
+                                </label>
+                                {errors.contract && <div className='text-red'>Заполните поле*</div>}
 
-                                    <label>Оплачено:
-                                    <input name='ispaid' className='form-input'  type="number"
-                                           {...register('ispaid', {required:true})}
+                                <label>Оплачено:
+                                    <input name='ispaid' className='form-input' type="number"
+                                           {...register('ispaid', {required: true})}
                                     />
-                                    </label>
-                                    {errors.ispaid && <div className='text-red'>Заполните поле*</div>}
-                                </div>
-
-                                <div className="form-group">
-                                    <label>Ноутбук:
-                                        <input name='notebook' className='form-input'  type="text"
-                                               {...register('notebook', {required:true})}
-                                        />
-                                    </label>
-                                    {errors.notebook && <div className='text-red'>Заполните поле*</div>}
-
-                                </div>
-                                <div className="form-group">
-                                    <label htmlFor="comments">Ваш комментарий:</label>
-                                    <textarea name="comments" id="comments" style={{width: "100%"}} cols='5' placeholder='Комментарий...'
-                                              {...register('comments')}
+                                </label>
+                                {errors.ispaid && <div className='text-red'>Заполните поле*</div>}
+                            </div>
+                            <div className="form-group">
+                                <label>Ноутбук:
+                                    <input name='notebook' className='form-input' type="text"
+                                           {...register('notebook', {required: true})}
                                     />
-                                    {errors.comments && <div className='text-red'>Заполните поле*</div>}
-                                </div>
-                                <button className='send-btn' type="submit">Добавить</button>
-                            </form>
+                                </label>
+                                {errors.notebook && <div className='text-red'>Заполните поле*</div>}
+                            </div>
+                            <div className="form-group d-flex justify-content-between">
+                                <label htmlFor="group">Выберите группу:
+                                    <div><select {...register('group', {required: true})} >
+                                        <option selected>Выберите группу</option>
+                                        <option value="Утренняя">Утренняя</option>
+                                        <option value="Вечерняя">Вечерняя</option>
+                                    </select>
+                                    </div>
+                                </label>
+                                {errors.group && <div className='text-red'>Выберите*</div>}
+                                <label htmlFor="gender">Выберите пол:
+                                    <div><select {...register('gender', {required: true})} >
+                                        <option selected>Выберите пол</option>
+                                        <option value="мужской">Мужской</option>
+                                        <option value="женский">Женский</option>
+                                    </select>
+                                    </div>
+                                </label>
+                                {errors.gender && <div className='text-red'>Выберите*</div>}
+                                <label htmlFor="status">Выберите статус:
+                                    <div><select {...register('status', {required: true})} >
+                                        <option selected>Выберите статус</option>
+                                        <option value="active">active</option>
+                                        <option value="off">off</option>
+                                    </select>
+                                    </div>
+                                </label>
+                                {errors.status && <div className='text-red'>Выберите статус*</div>}
+                            </div>
+
+                            <div className="form-group">
+                                <label htmlFor="comments">Ваш комментарий:</label>
+                                <textarea name="comments" id="comments" style={{width: "100%"}} cols='5'
+                                          placeholder='Комментарий...'
+                                          {...register('comments')}
+                                />
+                                {errors.comments && <div className='text-red'>Заполните поле*</div>}
+                            </div>
+                            <button className='send-btn' type="submit">Добавить</button>
+                        </form>
                     }
                     <button onClick={() => setIsOpen(false)} className="close-modal">&#215;</button>
                 </div>
